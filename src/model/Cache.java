35a98ba2073e88029672e19569d2a6dc41f95852
Vitaliy Radchishin <vitaliyrd@gmail.com>
2015-06-03 20:24:08 -0700

    Created the Memory class. Started the Cache class.

--- src/model/Cache.java
+++ src/model/Cache.java
@@ -1,7 +1,27 @@
 package model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by Vitaliy on 6/3/15.
  */
 public class Cache {
+    private int accesses;
+    private int misses;
+    private List<Integer> contents;
+
+    private int size;
+    private int blockSize;
+    private int latency;
+
+    public Cache(int size, int blockSize, int latency) {
+        accesses = 0;
+        misses = 0;
+        contents = new ArrayList<>();
+
+        this.size = size;
+        this.blockSize = blockSize;
+        this.latency = latency;
+    }
 }

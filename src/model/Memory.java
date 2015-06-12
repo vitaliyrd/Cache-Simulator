45ea11219a88c2fe65cbe530ffe891913ac1e731
Andy <thematulaaklives@gmail.com>
2015-06-12 13:00:22 -0700

    Created the Memory class.

--- src/model/Memory.java
+++ src/model/Memory.java
@@ -4,4 +4,22 @@ package model;
  * Created by Vitaliy on 6/3/15.
  */
 public class Memory {
+
+    private int reads;
+    private int writes;
+    private int size;
+    private int readLatency;
+    private int writeLatency;
+
+    public Memory() {
+
+    }
+
+    void read(int location) {
+
+    }
+
+    void write(int location) {
+
+    }
 }

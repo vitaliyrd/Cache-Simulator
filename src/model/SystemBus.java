db46194d0994d4968e9302a796ffde2c967d270c
Alexander Glass <glassa@uw.edu>
2015-06-14 18:39:14 -0700

    Update SystemBus.java

--- src/model/SystemBus.java
+++ src/model/SystemBus.java
@@ -6,18 +6,28 @@ package model;
  * @author Alex Glass, Vitaliy Radchishin, Andy Tran, Tru Truong
  * @version 1.0
  */
+ 
+ /**
+  * Which CPU has ownership
+  */
+public CPU owned;
 public class SystemBus {
-
+    
     public int issueReadRequest(int address, CPU caller) {
-
+        if (owned == caller|| owned == NULL){
         return 100; //(Alex, unsure) The latency penalty for having to read from memory
+        }
+    return 0;
     }
 
     public int issueWriteRequest(int address, CPU caller) {
-        return 100; //(Alex, unsure) The latency penalty for having to write to memory
+         if (owned == caller || owned == NULL){
+        return 100; //(Alex, unsure) The latency penalty for having to read from memory
+        }
+        return 0;
     }
 
     public void issueRequestForOwnership(int address, CPU caller) {
-
+            owned = caller
     }
 }
